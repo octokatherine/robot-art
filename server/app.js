18399d3f5dec4ea73ce2d8b9fd8e49edafebad7a
katherinepeterson <katherine.peterson@yahoo.com>
2021-05-07 17:03:41 -0700

    update paths to build folder and add build folder to gitignore

--- server/app.js
+++ server/app.js
@@ -4,10 +4,10 @@ const path = require('path')
 const app = express()
 
 app.use(express.json())
-app.use(express.static(path.join(__dirname, '..', 'public')))
+app.use(express.static(path.join(__dirname, '.', 'public')))
 
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'public', 'index.html'))
+  res.sendFile(path.join(__dirname, '.', 'public', 'index.html'))
 })
 
 module.exports = app
