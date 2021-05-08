4feea77f9bad9444ba9f34318122bbf5839affa2
katherinepeterson <katherine.peterson@yahoo.com>
2021-05-07 21:09:58 -0700

    hash passwords on save

--- server/routers/userRouter.js
+++ server/routers/userRouter.js
@@ -1,18 +1,22 @@
 const prisma = require('../prismaConnection')
 const express = require('express')
+const bcrypt = require('bcrypt')
+const saltRounds = 10
 
 const userRouter = express.Router()
 
 userRouter.post('/', async (req, res) => {
-  const user = await prisma.user.create({
-    data: {
-      fullName: req.body.fullname,
-      username: req.body.username,
-      password: req.body.password,
-      isAdmin: false,
-    },
+  bcrypt.hash(req.body.password, saltRounds, async function (err, hash) {
+    const user = await prisma.user.create({
+      data: {
+        fullname: req.body.fullname,
+        username: req.body.username,
+        password: hash,
+        isAdmin: false,
+      },
+    })
+    res.status(201).json(user)
   })
-  res.status(201).json(user)
 })
 
 module.exports = userRouter
