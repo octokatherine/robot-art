3cb281ce9f6a8dfd8dfbb54917e00de13d7b9ed1
katherinepeterson <katherine.peterson@yahoo.com>
2021-05-09 13:15:36 -0700

    add cleanup function to withAuth useEffect

--- client/src/components/withAuth.js
+++ client/src/components/withAuth.js
@@ -1,39 +1,41 @@
-import React, { Component } from 'react'
+import axios from 'axios'
+import { useState, useEffect } from 'react'
 import { Redirect } from 'react-router-dom'
 
-export default function withAuth(ComponentToProtect) {
-  return class extends Component {
-    constructor() {
-      super()
-      this.state = {
-        loading: true,
-        redirect: false,
-      }
+const withAuth = (ComponentToProtect) => (props) => {
+  const [loading, setLoading] = useState(true)
+  const [redirect, setRedirect] = useState(false)
+
+  useEffect(() => {
+    let unmounted = false
+    let source = axios.CancelToken.source()
+    axios
+      .get('/checkToken', {
+        cancelToken: source.token,
+      })
+      .then(() => {
+        if (!unmounted) setLoading(false)
+      })
+      .catch((err) => {
+        if (!unmounted) {
+          console.log(err)
+          setLoading(false)
+          setRedirect(true)
+        }
+      })
+    return function () {
+      unmounted = true
+      source.cancel('Cancelling in cleanup')
     }
-    componentDidMount() {
-      fetch('/checkToken')
-        .then((res) => {
-          if (res.status === 200) {
-            this.setState({ loading: false })
-          } else {
-            const error = new Error(res.error)
-            throw error
-          }
-        })
-        .catch((err) => {
-          console.error(err)
-          this.setState({ loading: false, redirect: true })
-        })
-    }
-    render() {
-      const { loading, redirect } = this.state
-      if (loading) {
-        return null
-      }
-      if (redirect) {
-        return <Redirect to="/login" />
-      }
-      return <ComponentToProtect {...this.props} />
-    }
+  }, [])
+
+  if (loading) {
+    return null
   }
+  if (redirect) {
+    return <Redirect to="/login" />
+  }
+  return <ComponentToProtect {...props} />
 }
+
+export default withAuth
