365c5ab7222a67fe82605019959fedd7446997f1
katherinepeterson <katherine.peterson@yahoo.com>
2021-05-08 21:01:30 -0700

    refactor class component to functional component

--- client/src/pages/Admin.js
+++ client/src/pages/Admin.js
@@ -1,23 +1,18 @@
-import React, { Component } from 'react'
+import { useState, useRef } from 'react'
 import axios from 'axios'
 
-class Admin extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      success: false,
-      url: '',
-    }
+const Admin = () => {
+  const uploadInput = useRef(null)
+  const [url, setUrl] = useState('')
+
+  const handleChange = (ev) => {
+    setUrl('')
   }
 
-  handleChange = (ev) => {
-    this.setState({ success: false, url: '' })
-  }
-  // Perform the upload
-  handleUpload = (ev) => {
-    let file = this.uploadInput.files[0]
+  const handleUpload = (ev) => {
+    let file = uploadInput.current.files[0]
     // Split the filename to get the name and type
-    let fileParts = this.uploadInput.files[0].name.split('.')
+    let fileParts = uploadInput.current.files[0].name.split('.')
     let fileName = fileParts[0]
     let fileType = fileParts[1]
     console.log('Preparing the upload')
@@ -30,10 +25,9 @@ class Admin extends Component {
         var returnData = response.data.data.returnData
         var signedRequest = returnData.signedRequest
         var url = returnData.url
-        this.setState({ url: url })
+        setUrl(url)
         console.log('Recieved a signed request ' + signedRequest)
 
-        // Put the fileType in the headers for the upload
         var options = {
           headers: {
             'Content-Type': fileType,
@@ -43,7 +37,7 @@ class Admin extends Component {
           .put(signedRequest, file, options)
           .then((result) => {
             console.log('Response from s3')
-            this.setState({ success: true })
+            console.log('response :>> ', response)
           })
           .catch((error) => {
             console.log('ERROR ' + JSON.stringify(error))
@@ -54,31 +48,15 @@ class Admin extends Component {
       })
   }
 
-  render() {
-    const Success_message = () => (
-      <div style={{ padding: 50 }}>
-        <h3 style={{ color: 'green' }}>SUCCESSFUL UPLOAD</h3>
-        <a href={this.state.url}>Access the file here</a>
+  return (
+    <div>
+      <center>
+        <h1>UPLOAD A FILE</h1>
+        <input onChange={handleChange} ref={uploadInput} type="file" />
         <br />
-      </div>
-    )
-    return (
-      <div>
-        <center>
-          <h1>UPLOAD A FILE</h1>
-          {this.state.success ? <Success_message /> : null}
-          <input
-            onChange={this.handleChange}
-            ref={(ref) => {
-              this.uploadInput = ref
-            }}
-            type="file"
-          />
-          <br />
-          <button onClick={this.handleUpload}>UPLOAD</button>
-        </center>
-      </div>
-    )
-  }
+        <button onClick={handleUpload}>UPLOAD</button>
+      </center>
+    </div>
+  )
 }
 export default Admin
