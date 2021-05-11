9a55a5e6eddc286383edb45d6cb2a21808bec753
katherinepeterson <katherine.peterson@yahoo.com>
2021-05-10 20:48:28 -0700

    move auth endpoints into router

--- server/app.js
+++ server/app.js
@@ -4,11 +4,7 @@ const cookieParser = require('cookie-parser')
 const userRouter = require('./routers/userRouter')
 const robotRouter = require('./routers/robotRouter')
 const voteRouter = require('./routers/voteRouter')
-const prisma = require('./prismaConnection')
-const bcrypt = require('bcrypt')
-const jwt = require('jsonwebtoken')
-const withAuth = require('./middleware')
-const { sign_s3 } = require('./awsUpload')
+const authRouter = require('./routers/authRouter')
 
 const app = express()
 
@@ -19,56 +15,6 @@ app.use(express.static(path.join(__dirname, '.', 'public')))
 app.use('/users', userRouter)
 app.use('/robots', robotRouter)
 app.use('/votes', voteRouter)
-
-app.post('/sign_s3', sign_s3)
-
-app.post('/login', async (req, res) => {
-  const { username, password } = req.body
-  const user = await prisma.user.findFirst({
-    where: {
-      username: username,
-    },
-  })
-  if (!user) return res.status(401).json({ error: 'Incorrect username or password' })
-
-  bcrypt.compare(password, user.password, (err, result) => {
-    if (err) {
-      res.status(500).json({ error: 'Internal error' })
-    } else if (!result) {
-      res.status(401).json({ error: 'Incorrect username or password' })
-    } else {
-      const payload = { username, fullname: user.fullname, userId: user.id }
-      const token = jwt.sign(payload, process.env.SECRET, {
-        expiresIn: '1h',
-      })
-      res.cookie('token', token, { httpOnly: true }).status(200).json({ token: token })
-    }
-  })
-})
-
-app.delete('/logout', async (req, res) => {
-  res.cookie('token', '').sendStatus(200)
-})
-
-app.get('/checkToken', withAuth, function (req, res) {
-  res.status(200).json({ token: req.cookies.token })
-})
-
-app.get('/checkAdmin', withAuth, async function (req, res) {
-  const user = await prisma.user.findFirst({
-    where: {
-      username: req.username,
-    },
-  })
-  if (user.isAdmin) {
-    return res.status(200).json({ admin: true })
-  } else {
-    return res.status(401).json({ error: 'user is not an admin' })
-  }
-})
-
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, '.', 'public', 'index.html'))
-})
+app.use(authRouter)
 
 module.exports = app
---
+++ server/routers/authRouter.js
@@ -0,0 +1,62 @@
+const express = require('express')
+const bcrypt = require('bcrypt')
+const path = require('path')
+const jwt = require('jsonwebtoken')
+const { sign_s3 } = require('../awsUpload')
+const withAuth = require('../middleware')
+const prisma = require('../prismaConnection')
+
+const authRouter = express.Router()
+
+authRouter.post('/sign_s3', sign_s3)
+
+authRouter.post('/login', async (req, res) => {
+  const { username, password } = req.body
+  const user = await prisma.user.findFirst({
+    where: {
+      username: username,
+    },
+  })
+  if (!user) return res.status(401).json({ error: 'Incorrect username or password' })
+
+  bcrypt.compare(password, user.password, (err, result) => {
+    if (err) {
+      res.status(500).json({ error: 'Internal error' })
+    } else if (!result) {
+      res.status(401).json({ error: 'Incorrect username or password' })
+    } else {
+      const payload = { username, fullname: user.fullname, userId: user.id }
+      const token = jwt.sign(payload, process.env.SECRET, {
+        expiresIn: '1h',
+      })
+      res.cookie('token', token, { httpOnly: true }).status(200).json({ token: token })
+    }
+  })
+})
+
+authRouter.delete('/logout', async (req, res) => {
+  res.cookie('token', '').sendStatus(200)
+})
+
+authRouter.get('/checkToken', withAuth, function (req, res) {
+  res.status(200).json({ token: req.cookies.token })
+})
+
+authRouter.get('/checkAdmin', withAuth, async function (req, res) {
+  const user = await prisma.user.findFirst({
+    where: {
+      username: req.username,
+    },
+  })
+  if (user.isAdmin) {
+    return res.status(200).json({ admin: true })
+  } else {
+    return res.status(401).json({ error: 'user is not an admin' })
+  }
+})
+
+authRouter.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, '.', 'public', 'index.html'))
+})
+
+module.exports = authRouter
