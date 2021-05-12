4edf228cd2ad7c840a762910f5df044f52cd292e
katherinepeterson <katherine.peterson@yahoo.com>
2021-05-11 17:17:25 -0700

    remove robots from seed script

--- server/seed.js
+++ server/seed.js
@@ -13,20 +13,6 @@ const seedData = async () => {
     .catch((err) => console.log(err))
   console.log('Seeded admin user')
 
-  await prisma.robot
-    .createMany({
-      data: [
-        { name: 'Rosie', image: 'https://robot-art.s3.amazonaws.com/rosie' },
-        { name: 'Megazord', image: 'https://robot-art.s3.amazonaws.com/megazord' },
-        { name: 'Wall-E', image: 'https://robot-art.s3.amazonaws.com/walle' },
-        { name: 'Bender', image: 'https://robot-art.s3.amazonaws.com/Bender' },
-        { name: 'Voltron', image: 'https://robot-art.s3.amazonaws.com/voltron' },
-      ],
-      skipDuplicates: true,
-    })
-    .catch((err) => console.log(err))
-  console.log('Seeded robots')
-
   process.exit()
 }
 
